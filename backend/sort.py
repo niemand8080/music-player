777c26fae29dcd008606c582a231073938662816
niemand8080 <benhamster443@gmail.com>
2024-11-24 14:11:28 +0100

    Media

--- backend/sort.py
+++ backend/sort.py
@@ -17,12 +17,10 @@ MUSIC_DIR = f"{ROOT_PATH}{os.environ.get('MUSIC_DIR')}"
 VIDEOS_DIR = f"{ROOT_PATH}{os.environ.get('VIDEOS_DIR')}"
 ENV_DIR = f"{ROOT_PATH}{os.environ.get('ENV_DIR')}"
 DB_FILE = f"{ENV_DIR}/data/music.db"
-LINKS_FILE = f"{ROOT_PATH}{os.environ.get('LINKS_FILE')}"
-VIDEO_LINKS_FILE = f"{ROOT_PATH}{os.environ.get('VIDEO_LINKS_FILE')}"
 
 def advanced_print(msg: str, goal: int, current: int):
   terminal_width = shutil.get_terminal_size().columns
-  max_name_length = terminal_width - 50
+  max_name_length = terminal_width - 15
   displayed_msg = (msg[:max_name_length-3] + "...") if len(msg) > max_name_length else msg
   progress_msg = f"({current}/{goal}) -> {displayed_msg}"
   print(f"{progress_msg:<{terminal_width}}", end='\r')
@@ -99,10 +97,62 @@ def generateUID(input: str, existing: List[str]):
   
   return uid
 
-async def write_db():
-  print("\033[36mWrite songs to DB\033[0m")
+async def update_db():
+  print("\033[36mWrite media to DB\033[0m")
   artists = {a.name: a.artist_id for a in await sql("SELECT * FROM artists", fetch_results=True)}
   artist_ids = [a.artist_id for a in await sql("SELECT * FROM artists", fetch_results=True)]
+  count = 0
+  for root, _, files in os.walk(VIDEOS_DIR):
+    file_count = len([1 for f in files if not f.startswith('.') and f.endswith('.mp4')])
+    for file in files:
+      if file.startswith('.') or not file.endswith('.mp4'):
+        continue
+      count += 1
+
+      advanced_print(f"getting track: {file}", file_count, count)
+
+      file_path = os.path.join(root, file)
+      comment = getMetadata(file_path, "comment")
+      yt_link = ''.join(comment.split('yt:')).split(';')[0]
+      track = comment.split('track:')[1]
+
+      # Check if a media with the track exists and continue if it does
+      if await sql("SELECT 1 FROM media m INNER JOIN media_data md ON md.track_id = m.track_id WHERE m.track_id = ?", [track], fetch_success=True):
+        continue
+      
+      advanced_print(f"insert media : {file}", file_count, count)
+      artist = getMetadata(file_path, "artist")
+      artist_id = artists.get(artist)
+
+      # Create artist if not exists
+      if artist_id == None:
+        artist_id = f"a{generateUID(artist, artist_ids)}"
+        artist_ids.append(artist_id)
+        artists[artist] = artist_id
+        await sql("INSERT INTO artists (name, artist_id) VALUES (?, ?)", [artist, artist_id])
+        print(f"Artist \033[34m{artist}\033[0m added: \033[33m{artist_id}\033[0m")
+
+      title = getMetadata(file_path, "title")
+      date = getMetadata(file_path, "date")
+      duration = getMetadata(file_path, "duration", "")
+      added = round(time.time() * 1000)
+      rel_path = os.path.basename(file)
+      tags = ""
+
+      await sql("""
+        INSERT or IGNORE INTO media
+        (name, artist_id, yt_link, date, track_id, duration, added, rel_path, type)
+        VALUES (?,?,?,?,?,?,?,?,?)
+      """, [title, artist_id, yt_link, date, track, duration, added, rel_path, "v"])
+
+      await sql("""
+        INSERT INTO media_data
+        (name, artist_id, track_id, tags, type)
+        VALUES (?, ?, ?, ?, ?)
+      """, [title, artist_id, track, tags, "v"])
+
+      print(f"Media \033[34m{title}\033[0m added: \033[33m{track}\033[0m")
+  return
   count = 0
   for root, _, files in os.walk(MUSIC_DIR):
     file_count = len([1 for file in files if not file.startswith('.') and file.endswith('.mp3')])
@@ -116,8 +166,8 @@ async def write_db():
       file_path = os.path.join(root, file)
       track = getMetadata(file_path, "track")
         
-      # Check if a song with the track exists and continue if it does
-      if await sql("SELECT 1 FROM songs s INNER JOIN songs_data sd ON sd.track_id = s.track_id WHERE s.track_id = ?", [track], fetch_success=True):
+      # Check if a media with the track exists and continue if it does
+      if await sql("SELECT 1 FROM media m INNER JOIN media_data md ON md.track_id = m.track_id WHERE m.track_id = ?", [track], fetch_success=True):
         continue
 
       advanced_print(f"insert into db: {file}", file_count, count)
@@ -143,97 +193,40 @@ async def write_db():
 
       if artist == "Suno":
         tags = "AI Generated"
-      else:
-        f = open(LINKS_FILE, "a")
-        f.write(f"\n{comment}")
-        f.close()
-
 
       await sql("""
-        INSERT or IGNORE INTO songs
+        INSERT or IGNORE INTO media
         (name, artist_id, yt_link, date, track_id, duration, added, rel_path)
         VALUES (?,?,?,?,?,?,?,?)
       """, [title, artist_id, comment, date, track, duration, added, rel_path])
 
       await sql("""
-        INSERT or IGNORE INTO songs_data
-        (name, artist_id, track_id, tags)
-        VALUES (?, ?, ?, ?)
-      """, [title, artist_id, track, tags])
+        INSERT or IGNORE INTO media_data
+        (name, artist_id, track_id, tags, type)
+        VALUES (?, ?, ?, ?, ?)
+      """, [title, artist_id, track, tags, "s"])
 
-      print(f"Song \033[34m{title}\033[0m added: \033[33m{track}\033[0m")
-  count = 0
-  for root, _, files in os.walk(VIDEOS_DIR):
-    file_count = len([1 for f in files if not f.startswith('.') and f.endswith('.mp4')])
-    for file in files:
-      if file.startswith('.') or not file.endswith('.mp4'):
-        continue
-      count += 1
+      print(f"Media \033[34m{title}\033[0m added: \033[33m{track}\033[0m")
 
-      advanced_print(f"getting track: {file}", file_count, count)
-
-      file_path = os.path.join(root, file)
-      comment = getMetadata(file_path, "comment")
-      yt_link = ''.join(comment.split('yt:')).split(';')[0]
-      track = comment.split('track:')[1]
-
-      # Check if a video with the track exists and continue if it does
-      if await sql("SELECT 1 FROM videos v INNER JOIN video_data vd ON vd.track_id = v.track_id WHERE v.track_id = ?", [track], fetch_success=True):
-        continue
-      
-      advanced_print(f"insert video : {file}", file_count, count)
-      artist = getMetadata(file_path, "artist")
-      artist_id = artists.get(artist)
-
-      # Create artist if not exists
-      if artist_id == None:
-        artist_id = generateUID(artist, artist_ids)
-        artist_ids.append(artist_id)
-        artists[artist] = artist_id
-        await sql("INSERT INTO artists (name, artist_id) VALUES (?, ?)", [artist, artist_id])
-        print(f"Artist \033[34m{artist}\033[0m added: \033[33m{artist_id}\033[0m")
-
-      title = getMetadata(file_path, "title")
-      date = getMetadata(file_path, "date")
-      duration = getMetadata(file_path, "duration", "")
-      added = round(time.time() * 1000)
-      rel_path = os.path.basename(file)
-      tags = ""
-
-      f = open(VIDEO_LINKS_FILE, "a")
-      f.write(f"\n{yt_link}")
-      f.close()
-
-      await sql("""
-        INSERT or IGNORE INTO videos
-        (name, artist_id, yt_link, date, track_id, duration, added, rel_path)
-        VALUES (?,?,?,?,?,?,?,?)
-      """, [title, artist_id, yt_link, date, track, duration, added, rel_path])
-
-      await sql("""
-        INSERT INTO video_data
-        (name, artist_id, track_id, tags)
-        VALUES (?, ?, ?, ?)
-      """, [title, artist_id, track, tags])
-
-      print(f"Video \033[34m{title}\033[0m added: \033[33m{track}\033[0m")
-
-async def check_db():
-  print("\033[36mCheck DB\033[0m")
-  await sql("UPDATE songs SET file_exists = 0")
-  songs = await sql("SELECT * FROM songs", fetch_results=True)
+async def check_files():
+  # missing videos in db????
+  print("\033[36mCheck Files\033[0m")
+  await sql("UPDATE media SET file_exists = 0")
+  media = await sql("SELECT * FROM media_data", fetch_results=True)
+  print(media[0].name)
   where_track = []
-  for song in songs:
-    file_path = os.path.join(MUSIC_DIR, song.rel_path)
+  return
+  for media in media:
+    file_path = os.path.join(MUSIC_DIR, media.rel_path)
     file_exists = os.path.exists(file_path)
     if file_exists:
-      where_track.append(f"track_id = '{song.track_id}'")
+      where_track.append(f"track_id = '{media.track_id}'")
 
-  await sql(f"UPDATE songs SET file_exists = 1 WHERE {' OR '.join([track for track in where_track])}")
+  await sql(f"UPDATE media SET file_exists = 1 WHERE {' OR '.join([track for track in where_track])}")
 
 async def main():
-  await write_db()
-  await check_db()
+  await update_db()
+  await check_files()
 
 
 if __name__ == "__main__":
